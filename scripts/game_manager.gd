4282d4cdbbb0097e1f24584589fb2f0c6fc42c9e
afonsojorge15 <afonso.matos.jorge@gmail.com>
2024-07-30 12:26:47 +0100

    adds pecentage of filled tiles win condition and progress bar

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -9,7 +9,6 @@ enum WinResult { PLAYER_1_WIN, PLAYER_2_WIN, TIE }
 var game_state: GameState 
 var current_player: Player 
 var turn_count: int = 0
-@export var MAX_TURNS: int = 2
 var winner: WinResult
 var winner_message: String
 
@@ -18,6 +17,10 @@ var winner_message: String
 @onready var combination_board_p2 : Control= $CombinationBoardP2
 @onready var game_manager = $"."
 @onready var menu_manager_node = $"../MenuManager"
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+var total_tiles: int
+var filled_tiles: int
 
 func _ready():
 	tile_map.connect("piece_placed", Callable(self, "_on_piece_placed"))
@@ -27,16 +30,19 @@ func _ready():
 	combination_board_p2.connect("combination_complete", Callable(self, "on_combination_complete"))
 	game_state = GameState.MENU
 	hide_combination_boards()
+	
+	var board_rect = tile_map.get_used_rect()
+	total_tiles = board_rect.size.x * board_rect.size.y - 2 * board_rect.size.y  # Subtract the two starting columns
 
 func game_started():
 	game_state = GameState.COMBINING
-	print_debug("game_started", game_state)
 	current_player = Player.PLAYER_1
-	print_debug("game_started", current_player)
 	turn_count = 0
 	tile_map.reset()
+	filled_tiles = 0
+	update_progress_bar()
 	show_combination_board(current_player)
-	
+
 func hide_combination_boards():
 	combination_board_p1.hide_board()
 	combination_board_p2.hide_board()
@@ -51,7 +57,6 @@ func show_combination_board(player: Player):
 		combination_board_p2.get_node("CombinationMap").reset()
 	game_state = GameState.COMBINING
 
-
 func on_combination_complete(combined_piece):
 	game_state = GameState.PLACING
 	tile_map.set_active_piece(combined_piece)
@@ -60,11 +65,21 @@ func on_combination_complete(combined_piece):
 func _on_piece_placed():
 	if game_state != GameState.PLACING:
 		return
-	end_turn()
+	filled_tiles += tile_map.active_piece.size()
+	update_progress_bar()
+	
+	if (filled_tiles / float(total_tiles)) >= 0.8:
+		end_game()
+	else:
+		end_turn()
+
+func update_progress_bar():
+	var fill_percentage = filled_tiles / float(total_tiles)
+	progress_bar.value = fill_percentage
 
 func end_game():
 	game_state = GameState.GAME_OVER
-	winner = determine_winner()  # Actually determine the winner
+	winner = determine_winner()
 	match winner:
 		WinResult.PLAYER_1_WIN:
 			winner_message = "Game Over! Player 1 wins!"
@@ -73,7 +88,6 @@ func end_game():
 		WinResult.TIE:
 			winner_message = "Game Over! It's a tie!"
 	print_debug(winner_message)
-	#ui.on_game_over(winner_message)
 	menu_manager_node.on_game_over(winner_message)
 
 func go_main_menu():
@@ -83,14 +97,11 @@ func go_main_menu():
 func end_turn():
 	turn_count += 1
 	print_debug("Turn " + str(turn_count) + " completed")
-	if turn_count >= MAX_TURNS:
-		end_game()
-	else:
-		switch_player()
-		game_state = GameState.COMBINING
-		show_combination_board(current_player)
-		tile_map.update_for_new_turn(current_player)
-		print_debug("Turn " + str(turn_count + 1) + " - Player " + str(current_player + 1) + "'s turn")
+	switch_player()
+	game_state = GameState.COMBINING
+	show_combination_board(current_player)
+	tile_map.update_for_new_turn(current_player)
+	print_debug("Turn " + str(turn_count + 1) + " - Player " + str(current_player + 1) + "'s turn")
 
 func switch_player():
 	current_player = Player.PLAYER_2 if current_player == Player.PLAYER_1 else Player.PLAYER_1
