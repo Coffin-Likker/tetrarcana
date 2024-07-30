3019ead5d77e2b7dd820b86e922b3d8d7bb20d82
Diogo <diogo@mercurylabs.io>
2024-07-30 23:57:50 +0100

    add parchment sound to game sound manager

--- scripts/GameSoundManager.gd
+++ scripts/GameSoundManager.gd
@@ -6,6 +6,7 @@ var win_sounds: Dictionary = {}
 var combine_sounds: Dictionary = {}
 var cant_place_sound: AudioStreamPlayer
 var current_sound_index: int = 0
+var open_combo_sound : AudioStreamPlayer
 
 func _ready():
 	# Initialize move sounds
@@ -31,35 +32,40 @@ func _ready():
 	
 	# Initialize win sounds
 	var light_win_sound = AudioStreamPlayer.new()
-	light_win_sound.stream = load("res://assets/audio/win/LightWin.wav")  # Adjust the path
+	light_win_sound.stream = load("res://assets/audio/winning/LightWin.wav")  # Adjust the path
 	light_win_sound.bus = AudioManager.SFX_BUS_NAME
 	add_child(light_win_sound)
 	win_sounds["light"] = light_win_sound
 	
 	var shadow_win_sound = AudioStreamPlayer.new()
-	shadow_win_sound.stream = load("res://assets/audio/win/ShadowWin.wav")  # Adjust the path
+	shadow_win_sound.stream = load("res://assets/audio/winning/ShadowWin.wav")  # Adjust the path
 	shadow_win_sound.bus = AudioManager.SFX_BUS_NAME
 	add_child(shadow_win_sound)
 	win_sounds["shadow"] = shadow_win_sound
 	
 	# Initialize combine sounds
 	var light_combine_sound = AudioStreamPlayer.new()
-	light_combine_sound.stream = load("res://assets/audio/combine/LightCombine.wav")  # Adjust the path
+	light_combine_sound.stream = load("res://assets/audio/placing/LightCombining.wav")  # Adjust the path
 	light_combine_sound.bus = AudioManager.SFX_BUS_NAME
 	add_child(light_combine_sound)
 	combine_sounds["light"] = light_combine_sound
 	
 	var shadow_combine_sound = AudioStreamPlayer.new()
-	shadow_combine_sound.stream = load("res://assets/audio/combine/ShadowCombine.wav")  # Adjust the path
+	shadow_combine_sound.stream = load("res://assets/audio/placing/ShadowCombining.wav")  # Adjust the path
 	shadow_combine_sound.bus = AudioManager.SFX_BUS_NAME
 	add_child(shadow_combine_sound)
 	combine_sounds["shadow"] = shadow_combine_sound
 	
 	# Initialize can't place sound
 	cant_place_sound = AudioStreamPlayer.new()
-	cant_place_sound.stream = load("res://assets/audio/cant_place/CantPlaceHere.wav")  # Adjust the path
+	cant_place_sound.stream = load("res://assets/audio/placing/CannotPlaceError.wav")  # Adjust the path
 	cant_place_sound.bus = AudioManager.SFX_BUS_NAME
 	add_child(cant_place_sound)
+	
+	open_combo_sound = AudioStreamPlayer.new()
+	open_combo_sound.stream = load("res://assets/audio/misc/PaperScroll.wav")
+	open_combo_sound.bus = AudioManager.SFX_BUS_NAME
+	add_child(open_combo_sound)
 
 func play_move_sound():
 	if move_sounds.size() > 0:
@@ -77,6 +83,9 @@ func play_win_sound(player: String):
 func play_combine_sound(player: String):
 	if player in combine_sounds:
 		combine_sounds[player].play()
+		
+func play_parchment_sound():
+	open_combo_sound.play()
 
 func play_cant_place_sound():
 	cant_place_sound.play()
